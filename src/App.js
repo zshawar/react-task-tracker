1bdf70974ec30713488ec23f3e8857ff388ea9a9
zshawar <zshawar@uncc.edu>
2021-08-16 21:10:44 -0400

    edits to app and header js files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
+
 function App() {
  
   return (
     <div className="container">
-      <h1>Hello From React</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = ({ title }) => {
+    return (
+        <header>
+            <h1>{title}</h1>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+export default Header
+
